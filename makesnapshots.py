fcd75b4cb67e2804e1964e834735b0cefcafaeae
Wouter Westenbrink <wouter@crowdpark.com>
2014-05-14 09:40:30 +0200

    remove sns publish, proxy option, search all regions for matching volumes

--- makesnapshots.py
+++ makesnapshots.py
@@ -20,8 +20,8 @@
 # version 3.2: Tags of the volume are placed on the new snapshot
 # version 3.3: Merged IAM role addidtion from Github
 
+import boto.ec2
 from boto.ec2.connection import EC2Connection
-from boto.ec2.regioninfo import RegionInfo
 import boto.sns
 from datetime import datetime
 import time
@@ -67,13 +67,7 @@ logging.info(start_message)
 # Get settings from config.py
 aws_access_key = config['aws_access_key']
 aws_secret_key = config['aws_secret_key']
-ec2_region_name = config['ec2_region_name']
-ec2_region_endpoint = config['ec2_region_endpoint']
 arn= config['arn']
-proxyHost = config['proxyHost']
-proxyPort = config['proxyPort']
-
-region = RegionInfo(name=ec2_region_name, endpoint=ec2_region_endpoint)
 
 # Number of snapshots to keep
 keep_week = config['keep_week']
@@ -81,37 +75,6 @@ keep_day = config['keep_day']
 keep_month = config['keep_month']
 count_succes = 0
 count_total = 0
-
-# Connect to AWS using the credentials provided above or in Environment vars or using IAM role.
-if proxyHost == '':
-	# non proxy:
-	# using roles
-	if aws_access_key == '':
-		conn = EC2Connection(region=region)
-	else:
-		conn = EC2Connection(aws_access_key,aws_secret_key,region=region)
-else:
-	# proxy:
-	# using roles
-	if aws_access_key == '':
-		conn = EC2Connection(region=region,proxy=proxyHost, proxy_port=proxyPort)
-	else:
-		conn = EC2Connection(aws_access_key,aws_secret_key,region=region,proxy=proxyHost, proxy_port=proxyPort)
-# Connect to SNS
-if proxyHost == '':
-	# non proxy:
-	# using roles
-	if aws_access_key == '':
-		sns = boto.sns.connect_to_region(ec2_region_name)
-	else:
-		sns = boto.sns.connect_to_region(ec2_region_name,aws_access_key_id=aws_access_key,aws_secret_access_key=aws_secret_key)
-else:
-	# proxy:
-	# using roles:
-	if aws_access_key == '':
-		sns = boto.sns.connect_to_region(ec2_region_name,proxy=proxyHost, proxy_port=proxyPort)
-	else:
-		sns = boto.sns.connect_to_region(ec2_region_name,aws_access_key_id=aws_access_key,aws_secret_access_key=aws_secret_key,proxy=proxyHost, proxy_port=proxyPort)
 
 def get_resource_tags(resource_id):
 		resource_tags = {}
@@ -127,68 +90,77 @@ def set_resource_tags(resource, tags):
 				print('Tagging '+ resource.id +' with ['+ tag_key +': '+ tag_value +']')
 				resource.add_tag(tag_key, tag_value)
 
-vols = conn.get_all_volumes(filters={config['tag_name']: config['tag_value']})
-for vol in vols:
-	try:
-		count_total += 1
-		logging.info(vol)
-		tags_volume = get_resource_tags(vol.id)
-		description = run + '_snapshot ' + vol.id + '_' + run + '_' + date_suffix + ' by snapshot script at ' + datetime.today().strftime('%d-%m-%Y %H:%M:%S')
-		try:
-			current_snap = vol.create_snapshot(description)
-			set_resource_tags(current_snap,tags_volume)
-			suc_message = 'Snapshot created with description: ' + str(description) + ' and tags: ' + str(tags_volume)
-			print '     ' + suc_message
-			logging.info(suc_message)
-			total_creates += 1
-		except Exception, e:
-			logging.error(e)
-			pass
+# search all regions
+regions = boto.ec2.regions(aws_access_key_id=aws_access_key,aws_secret_access_key=aws_secret_key)
+for region in regions:
+    conn = EC2Connection(aws_access_key,aws_secret_key,region=region)
+    try:
+        print "Looking for volumes in region "+region.name
+        vols = conn.get_all_volumes(filters={config['tag_name']: config['tag_value']})
+        for vol in vols:
+            print 'Backing up volume ' + vol + '\n';
+            try:
+                count_total += 1
+                logging.info(vol)
+                tags_volume = get_resource_tags(vol.id)
+                description = run + '_snapshot ' + vol.id + '_' + run + '_' + date_suffix + ' by snapshot script at ' + datetime.today().strftime('%d-%m-%Y %H:%M:%S')
+                try:
+                    current_snap = vol.create_snapshot(description)
+                    set_resource_tags(current_snap,tags_volume)
+                    suc_message = 'Snapshot created with description: ' + str(description) + ' and tags: ' + str(tags_volume)
+                    print '     ' + suc_message
+                    logging.info(suc_message)
+                    total_creates += 1
+                except Exception, e:
+                    logging.error(e)
+                    pass
 
-		snapshots = vol.snapshots()
-		deletelist = []
-		for snap in snapshots:
-			sndesc = snap.description
-			if (sndesc.startswith('week_snapshot') and run == 'week'):
-				deletelist.append(snap)
-			elif (sndesc.startswith('day_snapshot') and run == 'day'):
-				deletelist.append(snap)
-			elif (sndesc.startswith('month_snapshot') and run == 'month'):
-				deletelist.append(snap)
-			else:
-				logging.info('     Skipping, not added to deletelist: ' + sndesc)
-		for snap in deletelist:
-			logging.info(snap)
-			logging.info(snap.start_time)
+                snapshots = vol.snapshots()
+                deletelist = []
+                for snap in snapshots:
+                    sndesc = snap.description
+                    if (sndesc.startswith('week_snapshot') and run == 'week'):
+                        deletelist.append(snap)
+                    elif (sndesc.startswith('day_snapshot') and run == 'day'):
+                        deletelist.append(snap)
+                    elif (sndesc.startswith('month_snapshot') and run == 'month'):
+                        deletelist.append(snap)
+                    else:
+                        logging.info('     Skipping, not added to deletelist: ' + sndesc)
+                for snap in deletelist:
+                    logging.info(snap)
+                    logging.info(snap.start_time)
 
-		def date_compare(snap1, snap2):
-			if snap1.start_time < snap2.start_time:
-				return -1
-			elif snap1.start_time == snap2.start_time:
-				return 0
-			return 1
+                def date_compare(snap1, snap2):
+                    if snap1.start_time < snap2.start_time:
+                        return -1
+                    elif snap1.start_time == snap2.start_time:
+                        return 0
+                    return 1
 
-		deletelist.sort(date_compare)
-		if run=='day':
-			keep = keep_day
-		elif run=='week':
-			keep = keep_week
-		elif run=='month':
-			keep = keep_month
-		delta = len(deletelist) - keep
-		for i in range(delta):
-			del_message = '     Deleting snapshot ' + deletelist[i].description
-			logging.info(del_message)
-			deletelist[i].delete()
-			total_deletes += 1
-		time.sleep(3)
-	except:
-		print("Unexpected error:", sys.exc_info()[0])
-		logging.error('Error in processing volume with id: ' + vol.id)
-		errmsg += 'Error in processing volume with id: ' + vol.id
-		count_errors +=1
-	else:
-		count_succes +=1
+                deletelist.sort(date_compare)
+                if run=='day':
+                    keep = keep_day
+                elif run=='week':
+                    keep = keep_week
+                elif run=='month':
+                    keep = keep_month
+                delta = len(deletelist) - keep
+                for i in range(delta):
+                    del_message = '     Deleting snapshot ' + deletelist[i].description
+                    logging.info(del_message)
+                    deletelist[i].delete()
+                    total_deletes += 1
+                time.sleep(3)
+            except:
+                print("Unexpected error:", sys.exc_info()[0])
+                logging.error('Error in processing volume with id: ' + vol.id)
+                errmsg += 'Error in processing volume with id: ' + vol.id
+                count_errors +=1
+            else:
+                count_succes +=1
+    except:
+        print "Failed to retrieve volumes from region " + region.name
 
 result= '\nFinished making snapshots at ' + datetime.today().strftime('%d-%m-%Y %H:%M:%S') + ' with ' + str(count_succes) + ' snapshots of ' + str(count_total) + ' possible.'
 message += "\n" + "\n" + result
@@ -199,7 +171,4 @@ print '\n' + message + '\n'
 print result
 
 #Reporting
-if not errmsg == "":
-	sns.publish(arn,'Error in processing volumes: ' + errmsg,'Error with AWS Snapshot')
-sns.publish(arn,message,'Finished AWS snapshotting')
 logging.info(result)
